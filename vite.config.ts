f66dbfe5a28bb220e8403529afdac11728b24bf9
lkydy <tim.h4vlicek@gmail.com>
2026-01-31 05:12:16 +0100

    clean build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import tailwindcss from "@tailwindcss/vite";
-import Terminal from 'vite-plugin-terminal';
+// import Terminal from "vite-plugin-terminal";
 
 export default defineConfig({
-  base: '/alesis/',
-  plugins: [preact(), tailwindcss(), Terminal({
-    console: 'terminal',
-    output: ['terminal', 'console']
-  })],
-  server: {
-    port: 3000,
-  },
+	base: "/alesis/",
+	plugins: [
+		//
+		preact(),
+		tailwindcss(),
+		// Terminal({
+		// 	console: "terminal",
+		// 	output: ["terminal", "console"],
+		// }),
+	],
+	server: {
+		port: 3000,
+	},
 });
